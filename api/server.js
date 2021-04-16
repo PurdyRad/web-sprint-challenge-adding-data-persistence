7a7de0de089a459fe559082ea0ead5095da501ec
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-04-16 08:58:17 -0400

    Create a CodeGrade submission

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
-// build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get('*', (req,res) => {
+    res.send(`<h3>Ye'v got a fine vessel ready to sail</h3>`);
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// start your server here
+const server = require('./api/server');
+
+const PORT = 7110;
+
+server.listen(PORT, () => {
+    console.log(`Ye parrot be listening on port ${PORT}`);
+});
